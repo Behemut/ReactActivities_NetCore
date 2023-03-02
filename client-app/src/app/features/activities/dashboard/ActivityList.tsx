7afb121790644ee4385fc7c972aba995b9ebfc4a
Kevin Pleitez <kevin_eliu@hotmail.es>
2023-03-01 19:48:41 -0600

    Adding Axios Communication to API

--- client-app/src/app/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/app/features/activities/dashboard/ActivityList.tsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { SyntheticEvent, useState } from "react";
 import { Button, Item, Label, Segment } from "semantic-ui-react";
 import { Activity } from "../../../models/activity";
 
 
 interface Props {
     activities: Activity[]
-    selectActivity: (id: string) => void;
-    deleteActivity: (id: string) => void;
+    selectActivity: (id: string) => void
+    deleteActivity: (id: string) => void
+    submitting: boolean
 }
 
 
-export default function ActivityList({ activities, selectActivity, deleteActivity }: Props) {
+export default function ActivityList({ activities, selectActivity, deleteActivity, submitting }: Props) {
+   
+    const [target, setTarge] = useState('')
+
+    function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
+        setTarge(e.currentTarget.name)
+        deleteActivity(id)
+    }
+   
+   
     return (
         <Segment>
             <Item.Group divided>
@@ -25,7 +35,12 @@ export default function ActivityList({ activities, selectActivity, deleteActivit
                             </Item.Description>
                             <Item.Extra>
                                 <Button onClick={()=> selectActivity(activity.id)} floated='right' content='View' color='blue' />
-                                <Button onClick={()=> deleteActivity(activity.id)} floated='right' content='Delete' color='red' />
+                                <Button 
+                                    name={activity.id}
+                                    loading={submitting  && target === activity.id} 
+                                   // onClick={()=> deleteActivity(activity.id)} 
+                                    onClick={(e) => handleActivityDelete(e, activity.id)}
+                                    floated='right' content='Delete' color='red' />
                                 <Label basic content={activity.category} />
                             </Item.Extra>
                         </Item.Content>
--- client-app/src/app/features/activities/form/ActivityForm.tsx
+++ client-app/src/app/features/activities/form/ActivityForm.tsx
@@ -7,10 +7,11 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     activity: Activity | undefined;
+    submitting: boolean;
 }
 
 
-export default function ActivityForm({activity: selectedActivity, closeForm, createOrEdit}: Props) {
+export default function ActivityForm({activity: selectedActivity, closeForm, createOrEdit, submitting}: Props) {
 
     const initialFormState = selectedActivity ?? {
         id: '',
@@ -44,7 +45,7 @@ export default function ActivityForm({activity: selectedActivity, closeForm, cre
         <Form.Input type='date' placeholder='Date' value={activityForm.date} name="date" onChange={handleInputChange} />
         <Form.Input placeholder='City' value={activityForm.city} name="city" onChange={handleInputChange}/>
         <Form.Input placeholder='Venue' value={activityForm.venue} name="venue"  onChange={handleInputChange}/>
-        <Button floated="right" positive type='submit' content='Submit' />
+        <Button loading={submitting} floated="right" positive type='submit' content='Submit' />
         <Button onClick={closeForm} floated="right" color="red" type='button' content='Cancel' />
     </Form>
     </Segment>
